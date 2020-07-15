852855fc79cfd817d655226ac422a8f1c9a5e3d6
Idan <idan.zonshein@powerhrg.com>
2020-07-15 14:30:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+let multiply = num1 * num2
+
+let random = Math.ceil(Math.random())
+
+let mod = 24 % 5
+
+const max = Math.max(4, 20, 13, 10)
+
